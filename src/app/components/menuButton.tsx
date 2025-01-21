39dab872d4c620a098eb31ca0e31e03709b18e05
bryanfks-dev <bryanfernandoks11@gmail.com>
2025-01-21 18:52:28 +0700

    add menu button

---
+++ src/app/components/menuButton.tsx
@@ -0,0 +1,28 @@
+/**
+ * MenuButton is a button component that displays a menu icon.
+ *
+ * @returns {JSX.Element} MenuButton component
+ */
+export default function MenuButton(): JSX.Element {
+  return (
+    <button
+      type="button"
+      className="rounded-lg border px-4 hover:bg-gray-100 focus:ring focus:ring-emerald-500 focus:ring-opacity-50 md:px-6"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        className="size-8 text-gray-500"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+    </button>
+  );
+}
